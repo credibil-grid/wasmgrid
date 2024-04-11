0fb904b093b9dfde36b1b5344bfb07f934560728
andrewweston <andrew.weston@propellerhead.co.nz>
2024-04-12 10:50:56 +1200

    simplify main

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod nats;
 use anyhow::Error;
 pub use async_nats::Client;
 use clap::Parser;
+use futures::stream;
 use futures::stream::StreamExt;
 // use tokio::signal::unix::{signal, SignalKind};
 use wasi::messaging::messaging_types::HostClient;
@@ -37,58 +38,47 @@ struct Args {
 #[tokio::main]
 pub async fn main() -> wasmtime::Result<()> {
     // let args = Args::parse();
-    // let file = "target/wasm32-wasi/release/guest.wasm";
     let wasm = include_bytes!("../target/wasm32-wasi/release/guest.wasm");
-    let mut host_state = HostState::new();
-
-    // ---------------------------------------------------------------------
-    // TODO: move to nats package
-    // ---------------------------------------------------------------------
-    let client = host_state.connect("demo.nats.io".to_string()).await?.unwrap();
-    let client = host_state.client(&client)?;
-    // ---------------------------------------------------------------------
 
     let mut config = Config::new();
     config.wasm_component_model(true);
     config.async_support(true);
     let engine = Engine::new(&config)?;
-    let mut linker = Linker::new(&engine);
 
+    let mut linker = Linker::new(&engine);
     command::add_to_linker(&mut linker)?;
     messaging_types::add_to_linker(&mut linker, |t| t)?;
     producer::add_to_linker(&mut linker, |t| t)?;
     consumer::add_to_linker(&mut linker, |t| t)?;
 
-    // let component = Component::from_file(&engine, file)?;
     let component = Component::from_binary(&engine, wasm)?;
+
+    let mut host_state = HostState::new();
+
+    // TODO: move to nats package
+    let client = host_state.connect("demo.nats.io".to_string()).await?.unwrap();
+    let client = host_state.client(&client)?;
 
     let mut store = Store::new(&engine, host_state);
     let (messaging, _instance) =
         Messaging::instantiate_async(&mut store, &component, &linker).await?;
+
     let guest = messaging.wasi_messaging_messaging_guest();
-
-    // get channels guest wants to subscribe to
-    // N.B. As soon as configuration is retrieved, we should kill the wasm instance.
     let gc = guest.call_configure(store.as_context_mut()).await?;
-
     let mut subscribers = vec![];
     for ch in &gc.unwrap().channels {
         let subscriber = client.subscribe(ch.to_owned()).await?;
         subscribers.push(subscriber);
     }
 
-    let mut messages = futures::stream::select_all(subscribers);
-
+    let mut messages = stream::select_all(subscribers);
     while let Some(message) = messages.next().await {
         let msg = Message {
             data: message.payload.to_vec(),
             metadata: Some(vec![(String::from("channel"), message.subject.to_string())]),
             format: FormatSpec::Raw,
         };
-        let _ = messaging
-            .wasi_messaging_messaging_guest()
-            .call_handler(store.as_context_mut(), &[msg])
-            .await?;
+        let _ = guest.call_handler(store.as_context_mut(), &[msg]).await?;
     }
 
     Ok::<(), Error>(())
