481d82100f025bc30bc1371315dd2aecbfb6e7e8
andrewweston <andrew.weston@propellerhead.co.nz>
2024-04-16 13:20:29 +1200

    lint

--- src/messaging.rs
+++ src/messaging.rs
@@ -81,10 +81,10 @@ impl Client {
     }
 
     pub async fn subscribe(&self, ch: String) -> anyhow::Result<async_nats::Subscriber> {
-        Ok(self.inner.subscribe(ch).await?)
+        self.inner.subscribe(ch).await
     }
 
     pub async fn publish(&self, ch: String, data: Bytes) -> anyhow::Result<()> {
-        Ok(self.inner.publish(ch, data).await?)
+        self.inner.publish(ch, data).await
     }
 }
