75f1f614956e09d78545f9c54007d9c0cbdbc801
andrewweston <andrew.weston@propellerhead.co.nz>
2024-04-25 11:17:45 +1200

    reorg http runtime

--- src/http.rs
+++ src/http.rs
@@ -68,19 +68,17 @@ async fn handle_request(
     // let req_id = self.next_id.fetch_add(1, Ordering::Relaxed);
 
     let system = system.clone();
-    
-    let task = tokio::spawn(async move {
-        let mut store = system.store();
 
+    let task = tokio::spawn(async move {
         let (parts, body) = request.into_parts();
         let req = hyper::Request::from_parts(parts, body.map_err(hyper_response_error).boxed());
 
+        let mut store = system.store();
         let req = store.data_mut().new_incoming_request(req)?;
         let out = store.data_mut().new_response_outparam(sender)?;
 
-        let (proxy, _) = Proxy::instantiate_pre(&mut store, system.instance_pre()).await?;
-
         // call guest with request
+        let (proxy, _) = Proxy::instantiate_pre(&mut store, system.instance_pre()).await?;
         proxy.wasi_http_incoming_handler().call_handle(&mut store, req, out).await
     });
 
@@ -88,13 +86,7 @@ async fn handle_request(
         Ok(Ok(resp)) => Ok(resp),
         Ok(Err(e)) => Err(e.into()),
         Err(_) => {
-            // An error in the receiver (`RecvError`) only indicates that the
-            // task exited before a response was sent (i.e., the sender was
-            // dropped); it does not describe the underlying cause of failure.
-            // Instead we retrieve and propagate the error from inside the task
-            // which should more clearly tell the user what went wrong. Note
-            // that we assume the task has already exited at this point so the
-            // `await` should resolve immediately.
+            // Retrieve the actual task error.
             let e = match task.await {
                 Ok(r) => r.expect_err("if the receiver has an error, the task must have failed"),
                 Err(e) => e.into(),
