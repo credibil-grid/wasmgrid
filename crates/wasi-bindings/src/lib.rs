41f2cca8e2fa15ced340ae2c4d59c461919f8b0f
andrewweston <andrew.weston@propellerhead.co.nz>
2024-05-04 12:52:15 +1200

    fix bindings

--- crates/wasi-bindings/src/lib.rs
+++ crates/wasi-bindings/src/lib.rs
@@ -1,6 +1,6 @@
 //! # WASI Bindings
 //!
-//! This generates and exports WASI Guest bindings for local wit worlds.
+//! This module generates and exports WASI Guest bindings for local wit worlds.
 //! The bindings are exported in as similar a manner to those in the Bytecode
 //! Alliance's [wasi] crate.
 //!
