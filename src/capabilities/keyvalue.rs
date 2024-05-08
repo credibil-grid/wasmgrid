f90c020305bce4037b14ecb6731655e3b34019e1
andrewweston <andrew.weston@propellerhead.co.nz>
2024-05-08 17:41:17 +1200

    simplify keyvalue

--- src/capabilities/keyvalue.rs
+++ src/capabilities/keyvalue.rs
@@ -17,11 +17,12 @@ use wasmtime_wasi::WasiView;
 
 use crate::runtime::{self, Runtime, State};
 
+pub type Bucket = async_nats::jetstream::kv::Store;
 static JETSTREAM: OnceLock<jetstream::Context> = OnceLock::new();
 
 mod bindings {
     #![allow(clippy::future_not_send)]
-    pub use async_nats::jetstream::kv::Store as Bucket;
+    pub use super::Bucket;
 
     wasmtime::component::bindgen!({
         world: "keyvalue",
@@ -66,46 +67,26 @@ impl runtime::Capability for Capability {
     }
 }
 
-// Create a new Bucket for the specified NATS server.
-async fn new_bucket(
-    jetstream: &jetstream::Context, identifier: String,
-) -> anyhow::Result<bindings::Bucket> {
-    let inner = jetstream
-        .create_key_value(jetstream::kv::Config {
-            bucket: identifier.clone(),
-            history: 10,
-            ..Default::default()
-        })
-        .await?;
-
-    Ok(inner)
-}
-
-impl State {
-    // Get underlying JetStream Store from bucket Resource.
-    fn store(
-        &mut self, bucket: &Resource<bindings::Bucket>,
-    ) -> anyhow::Result<jetstream::kv::Store> {
-        let bucket = self.table().get_mut(bucket)?;
-        Ok(bucket.clone())
-    }
-}
-
 // Implement the [`wasi_keyvalue::KeyValueView`]` trait for State.
 #[async_trait::async_trait]
 impl store::Host for State {
     // Open bucket specified by identifier, save to state and return as a resource.
     async fn open(
         &mut self, identifier: String,
-    ) -> wasmtime::Result<Result<Resource<bindings::Bucket>, store::Error>> {
-        tracing::debug!("StoreView::open {identifier}");
+    ) -> wasmtime::Result<Result<Resource<Bucket>, store::Error>> {
+        tracing::debug!("store::Host::open {identifier}");
 
         let Some(jetstream) = JETSTREAM.get() else {
             return Err(anyhow!("JetStream not initialized"));
         };
 
-        // open bucket and save to state
-        let bucket = new_bucket(jetstream, identifier.clone()).await?;
+        let bucket = jetstream
+            .create_key_value(jetstream::kv::Config {
+                bucket: identifier.clone(),
+                history: 10,
+                ..Default::default()
+            })
+            .await?;
 
         Ok(Ok(self.table().push(bucket)?))
     }
@@ -114,67 +95,62 @@ impl store::Host for State {
 #[async_trait::async_trait]
 impl store::HostBucket for State {
     async fn get(
-        &mut self, bucket: Resource<bindings::Bucket>, key: String,
+        &mut self, rep: Resource<Bucket>, key: String,
     ) -> wasmtime::Result<Result<Option<Vec<u8>>, store::Error>> {
-        tracing::debug!("BucketView::get {key}");
-
-        let store = self.store(&bucket)?;
-        Ok(Ok(store.get(key).await?.map(|v| v.to_vec())))
+        tracing::debug!("store::HostBucket::get {key}");
+        let bucket = self.table().get_mut(&rep)?;
+        Ok(Ok(bucket.get(key).await?.map(|v| v.to_vec())))
     }
 
     async fn set(
-        &mut self, bucket: Resource<bindings::Bucket>, key: String, value: Vec<u8>,
+        &mut self, rep: Resource<Bucket>, key: String, value: Vec<u8>,
     ) -> wasmtime::Result<Result<(), store::Error>, wasmtime::Error> {
-        tracing::debug!("BucketView::set {key}");
-
-        let store = self.store(&bucket)?;
-        Ok(Ok(store.put(key, Bytes::from(value)).await.map(|_| ())?))
+        tracing::debug!("store::HostBucket::set {key}");
+        let bucket = self.table().get_mut(&rep)?;
+        Ok(Ok(bucket.put(key, Bytes::from(value)).await.map(|_| ())?))
     }
 
     async fn delete(
-        &mut self, bucket: Resource<bindings::Bucket>, key: String,
+        &mut self, rep: Resource<Bucket>, key: String,
     ) -> Result<Result<(), store::Error>, wasmtime::Error> {
-        tracing::debug!("BucketView::delete {key}");
-
-        let store = self.store(&bucket)?;
-        Ok(Ok(store.delete(key).await?))
+        tracing::debug!("store::HostBucket::delete {key}");
+        let bucket = self.table().get_mut(&rep)?;
+        Ok(Ok(bucket.delete(key).await?))
     }
 
     async fn exists(
-        &mut self, bucket: Resource<bindings::Bucket>, key: String,
+        &mut self, rep: Resource<Bucket>, key: String,
     ) -> wasmtime::Result<Result<bool, store::Error>> {
-        tracing::debug!("BucketView::exists {key}");
-
-        let store = self.store(&bucket)?;
-        Ok(Ok(store.get(key).await?.is_some()))
+        tracing::debug!("store::HostBucket::exists {key}");
+        let bucket = self.table().get_mut(&rep)?;
+        Ok(Ok(bucket.get(key).await?.is_some()))
     }
 
     async fn list_keys(
-        &mut self, bucket: Resource<bindings::Bucket>, cursor: Option<u64>,
+        &mut self, rep: Resource<Bucket>, cursor: Option<u64>,
     ) -> Result<Result<KeyResponse, store::Error>, wasmtime::Error> {
-        tracing::debug!("BucketView::list_keys {cursor:?}");
-
-        let store = self.store(&bucket)?;
-        let keys = store.keys().await?.try_collect::<Vec<String>>().await?;
+        tracing::debug!("store::HostBucket::list_keys {cursor:?}");
+        let bucket = self.table().get_mut(&rep)?;
+        let keys = bucket.keys().await?.try_collect::<Vec<String>>().await?;
         Ok(Ok(KeyResponse { keys, cursor }))
     }
 
     // LATER: Can a JetStream bucket be closed?
-    fn drop(&mut self, _: Resource<bindings::Bucket>) -> Result<(), wasmtime::Error> {
-        tracing::debug!("BucketView::drop");
-        Ok(())
+    fn drop(&mut self, rep: Resource<Bucket>) -> Result<(), wasmtime::Error> {
+        tracing::debug!("store::HostBucket::drop");
+        self.table().delete(rep).map_or_else(|e| Err(anyhow!(e)), |_| Ok(()))
     }
 }
 
 #[async_trait::async_trait]
 impl atomics::Host for State {
     async fn increment(
-        &mut self, bucket: Resource<bindings::Bucket>, key: String, delta: u64,
+        &mut self, rep: Resource<Bucket>, key: String, delta: u64,
     ) -> wasmtime::Result<Result<u64, Error>> {
-        tracing::debug!("AtomicsView::increment {key}, {delta}");
+        tracing::debug!("atomics::Host::increment {key}, {delta}");
 
-        let store = self.store(&bucket)?;
-        let value = store.get(key.clone()).await?.unwrap_or_default();
+        let bucket = self.table().get_mut(&rep)?;
+        let value = bucket.get(key.clone()).await?.unwrap_or_default();
 
         // increment value by delta
         let slice: &[u8] = &value;
@@ -183,7 +159,7 @@ impl atomics::Host for State {
         buf[..len].copy_from_slice(&slice[..len]);
         let inc = u64::from_be_bytes(buf) + delta;
 
-        store.put(key, Bytes::from((inc).to_be_bytes().to_vec())).await?;
+        bucket.put(key, Bytes::from((inc).to_be_bytes().to_vec())).await?;
 
         Ok(Ok(inc))
     }
@@ -192,44 +168,44 @@ impl atomics::Host for State {
 #[async_trait::async_trait]
 impl batch::Host for State {
     async fn get_many(
-        &mut self, bucket: Resource<bindings::Bucket>, keys: Vec<String>,
+        &mut self, rep: Resource<Bucket>, keys: Vec<String>,
     ) -> wasmtime::Result<Result<Vec<Option<(String, Vec<u8>)>>, store::Error>> {
-        tracing::debug!("BatchView::get_many {keys:?}");
+        tracing::debug!("batch::Host::get_many {keys:?}");
 
-        let store = self.store(&bucket)?;
+        let bucket = self.table().get_mut(&rep)?;
 
-        let mut results = Vec::new();
+        let mut many = Vec::new();
         for key in keys {
-            let value = store.get(&key).await?;
+            let value = bucket.get(&key).await?;
             if let Some(value) = value {
-                results.push(Some((key, value.to_vec())));
+                many.push(Some((key, value.to_vec())));
             }
         }
 
-        Ok(Ok(results))
+        Ok(Ok(many))
     }
 
     async fn set_many(
-        &mut self, bucket: Resource<bindings::Bucket>, key_values: Vec<(String, Vec<u8>)>,
+        &mut self, rep: Resource<Bucket>, key_values: Vec<(String, Vec<u8>)>,
     ) -> wasmtime::Result<Result<(), store::Error>> {
-        tracing::debug!("BatchView::set_many {key_values:?}");
+        tracing::debug!("batch::Host::set_many {key_values:?}");
 
-        let store = self.store(&bucket)?;
+        let bucket = self.table().get_mut(&rep)?;
         for (key, value) in key_values {
-            store.put(key, Bytes::from(value)).await?;
+            bucket.put(key, Bytes::from(value)).await?;
         }
 
         Ok(Ok(()))
     }
 
     async fn delete_many(
-        &mut self, bucket: Resource<bindings::Bucket>, keys: Vec<String>,
+        &mut self, rep: Resource<Bucket>, keys: Vec<String>,
     ) -> wasmtime::Result<Result<(), store::Error>> {
-        tracing::debug!("BatchView::delete_many {keys:?}");
+        tracing::debug!("batch::Host::delete_many {keys:?}");
 
-        let store = self.store(&bucket)?;
+        let bucket = self.table().get_mut(&rep)?;
         for key in keys {
-            store.delete(key).await?;
+            bucket.delete(key).await?;
         }
 
         Ok(Ok(()))
