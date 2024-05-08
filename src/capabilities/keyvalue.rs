813295e76ee507e7d7cad42068e38e632b280f8f
andrewweston <andrew.weston@propellerhead.co.nz>
2024-05-08 13:59:06 +1200

    remove unnecessary keyvalue host crate

--- src/capabilities/keyvalue.rs
+++ src/capabilities/keyvalue.rs
@@ -7,20 +7,36 @@ use std::sync::OnceLock;
 
 use anyhow::anyhow;
 use async_nats::jetstream;
+use bindings::wasi::keyvalue::store::{self, Error, KeyResponse};
+use bindings::wasi::keyvalue::{atomics, batch};
+use bindings::Keyvalue;
 use bytes::Bytes;
 use futures::TryStreamExt;
-use wasi_keyvalue::atomics::AtomicsView;
-use wasi_keyvalue::batch::BatchView;
-use wasi_keyvalue::bindings::wasi::keyvalue::store::KeyResponse;
-use wasi_keyvalue::bindings::Keyvalue;
-use wasi_keyvalue::store::{BucketView, StoreView};
-use wasi_keyvalue::{self, RuntimeBucket};
 use wasmtime::component::{Linker, Resource};
 use wasmtime_wasi::WasiView;
 
 use crate::runtime::{self, Runtime, State};
 
 static JETSTREAM: OnceLock<jetstream::Context> = OnceLock::new();
+
+mod bindings {
+    #![allow(clippy::future_not_send)]
+    // pub use super::Bucket;
+    pub use async_nats::jetstream::kv::Store as Bucket;
+
+    wasmtime::component::bindgen!({
+        world: "keyvalue",
+        path: "wit",
+        tracing: true,
+        async: true,
+        with: {
+            "wasi:keyvalue/store/bucket": Bucket,
+        },
+        // trappable_error_type: {
+        //     "wasi:keyvalue/keyvalue-types/error" => Error,
+        // },
+    });
+}
 
 pub struct Capability {
     pub addr: String,
@@ -53,24 +69,21 @@ impl runtime::Capability for Capability {
 
 impl State {
     // Get underlying JetStream Store from bucket Resource.
-    fn jetstream(
-        &mut self, bucket: &Resource<wasi_keyvalue::Bucket>,
+    fn store(
+        &mut self, bucket: &Resource<bindings::Bucket>,
     ) -> anyhow::Result<jetstream::kv::Store> {
         let bucket = self.table().get_mut(bucket)?;
-        let Some(bkt) = bucket.as_ref().as_any().downcast_ref::<Bucket>() else {
-            return Err(anyhow!("invalid JetStream store"));
-        };
-        Ok(bkt.0.clone())
+        Ok(bucket.clone())
     }
 }
 
 // Implement the [`wasi_keyvalue::KeyValueView`]` trait for State.
 #[async_trait::async_trait]
-impl StoreView for State {
+impl store::Host for State {
     // Open bucket specified by identifier, save to state and return as a resource.
     async fn open(
         &mut self, identifier: String,
-    ) -> anyhow::Result<Resource<wasi_keyvalue::Bucket>> {
+    ) -> wasmtime::Result<Result<Resource<bindings::Bucket>, store::Error>> {
         tracing::debug!("StoreView::open {identifier}");
 
         let Some(jetstream) = JETSTREAM.get() else {
@@ -78,74 +91,76 @@ impl StoreView for State {
         };
 
         // open bucket and save to state
-        let bucket = Bucket::new(jetstream, identifier.clone()).await?;
-        let boxed: wasi_keyvalue::Bucket = Box::new(bucket);
-
-        Ok(self.table().push(boxed)?)
+        let bucket = new_bucket(jetstream, identifier.clone()).await?;
+        
+        Ok(Ok(self.table().push(bucket)?))
     }
 }
 
 #[async_trait::async_trait]
-impl BucketView for State {
+impl store::HostBucket for State {
     async fn get(
-        &mut self, bucket: Resource<wasi_keyvalue::Bucket>, key: String,
-    ) -> anyhow::Result<Option<Vec<u8>>> {
+        &mut self, bucket: Resource<bindings::Bucket>, key: String,
+    ) -> wasmtime::Result<Result<Option<Vec<u8>>, store::Error>> {
         tracing::debug!("BucketView::get {key}");
 
-        let jetstream = self.jetstream(&bucket)?;
-
-        Ok(jetstream.get(key).await?.map(|v| v.to_vec()))
+        let store = self.store(&bucket)?;
+        Ok(Ok(store.get(key).await?.map(|v| v.to_vec())))
     }
 
     async fn set(
-        &mut self, bucket: Resource<wasi_keyvalue::Bucket>, key: String, value: Vec<u8>,
-    ) -> anyhow::Result<()> {
+        &mut self, bucket: Resource<bindings::Bucket>, key: String, value: Vec<u8>,
+    ) -> wasmtime::Result<Result<(), store::Error>, wasmtime::Error> {
         tracing::debug!("BucketView::set {key}");
-        let jetstream = self.jetstream(&bucket)?;
-        Ok(jetstream.put(key, Bytes::from(value)).await.map(|_| ())?)
+
+        let store = self.store(&bucket)?;
+        Ok(Ok(store.put(key, Bytes::from(value)).await.map(|_| ())?))
     }
 
     async fn delete(
-        &mut self, bucket: Resource<wasi_keyvalue::Bucket>, key: String,
-    ) -> anyhow::Result<()> {
+        &mut self, bucket: Resource<bindings::Bucket>, key: String,
+    ) -> Result<Result<(), store::Error>, wasmtime::Error> {
         tracing::debug!("BucketView::delete {key}");
-        let jetstream = self.jetstream(&bucket)?;
-        Ok(jetstream.delete(key).await?)
+
+        let store = self.store(&bucket)?;
+        Ok(Ok(store.delete(key).await?))
     }
 
     async fn exists(
-        &mut self, bucket: Resource<wasi_keyvalue::Bucket>, key: String,
-    ) -> anyhow::Result<bool> {
+        &mut self, bucket: Resource<bindings::Bucket>, key: String,
+    ) -> wasmtime::Result<Result<bool, store::Error>> {
         tracing::debug!("BucketView::exists {key}");
-        let jetstream = self.jetstream(&bucket)?;
-        Ok(jetstream.get(key).await?.is_some())
+
+        let store = self.store(&bucket)?;
+        Ok(Ok(store.get(key).await?.is_some()))
     }
 
     async fn list_keys(
-        &mut self, bucket: Resource<wasi_keyvalue::Bucket>, cursor: Option<u64>,
-    ) -> anyhow::Result<KeyResponse> {
+        &mut self, bucket: Resource<bindings::Bucket>, cursor: Option<u64>,
+    ) -> Result<Result<KeyResponse, store::Error>, wasmtime::Error> {
         tracing::debug!("BucketView::list_keys {cursor:?}");
-        let jetstream = self.jetstream(&bucket)?;
-        let keys = jetstream.keys().await?.try_collect::<Vec<String>>().await?;
-        Ok(KeyResponse { keys, cursor })
+
+        let store = self.store(&bucket)?;
+        let keys = store.keys().await?.try_collect::<Vec<String>>().await?;
+        Ok(Ok(KeyResponse { keys, cursor }))
     }
 
     // LATER: Can a JetStream bucket be closed?
-    fn drop(&mut self, _: Resource<wasi_keyvalue::Bucket>) -> anyhow::Result<()> {
+    fn drop(&mut self, _: Resource<bindings::Bucket>) -> Result<(), wasmtime::Error> {
         tracing::debug!("BucketView::drop");
         Ok(())
     }
 }
 
 #[async_trait::async_trait]
-impl AtomicsView for State {
+impl atomics::Host for State {
     async fn increment(
-        &mut self, bucket: Resource<wasi_keyvalue::Bucket>, key: String, delta: u64,
-    ) -> anyhow::Result<u64> {
+        &mut self, bucket: Resource<bindings::Bucket>, key: String, delta: u64,
+    ) -> wasmtime::Result<Result<u64, Error>> {
         tracing::debug!("AtomicsView::increment {key}, {delta}");
 
-        let jetstream = self.jetstream(&bucket)?;
-        let value = jetstream.get(key.clone()).await?.unwrap_or_default();
+        let store = self.store(&bucket)?;
+        let value = store.get(key.clone()).await?.unwrap_or_default();
 
         // increment value by delta
         let slice: &[u8] = &value;
@@ -154,83 +169,70 @@ impl AtomicsView for State {
         buf[..len].copy_from_slice(&slice[..len]);
         let inc = u64::from_be_bytes(buf) + delta;
 
-        jetstream.put(key, Bytes::from((inc).to_be_bytes().to_vec())).await?;
+        store.put(key, Bytes::from((inc).to_be_bytes().to_vec())).await?;
 
-        Ok(inc)
+        Ok(Ok(inc))
     }
 }
 
 #[async_trait::async_trait]
-impl BatchView for State {
+impl batch::Host for State {
     async fn get_many(
-        &mut self, bucket: Resource<wasi_keyvalue::Bucket>, keys: Vec<String>,
-    ) -> anyhow::Result<Vec<Option<(String, Vec<u8>)>>> {
+        &mut self, bucket: Resource<bindings::Bucket>, keys: Vec<String>,
+    ) -> wasmtime::Result<Result<Vec<Option<(String, Vec<u8>)>>, store::Error>> {
         tracing::debug!("BatchView::get_many {keys:?}");
 
-        let jetstream = self.jetstream(&bucket)?;
+        let store = self.store(&bucket)?;
 
         let mut results = Vec::new();
         for key in keys {
-            let value = jetstream.get(&key).await?;
+            let value = store.get(&key).await?;
             if let Some(value) = value {
                 results.push(Some((key, value.to_vec())));
             }
         }
 
-        Ok(results)
+        Ok(Ok(results))
     }
 
     async fn set_many(
-        &mut self, bucket: Resource<wasi_keyvalue::Bucket>, key_values: Vec<(String, Vec<u8>)>,
-    ) -> anyhow::Result<()> {
+        &mut self, bucket: Resource<bindings::Bucket>, key_values: Vec<(String, Vec<u8>)>,
+    ) -> wasmtime::Result<Result<(), store::Error>> {
         tracing::debug!("BatchView::set_many {key_values:?}");
 
-        let jetstream = self.jetstream(&bucket)?;
+        let store = self.store(&bucket)?;
         for (key, value) in key_values {
-            jetstream.put(key, Bytes::from(value)).await?;
+            store.put(key, Bytes::from(value)).await?;
         }
 
-        Ok(())
+        Ok(Ok(()))
     }
 
     async fn delete_many(
-        &mut self, bucket: Resource<wasi_keyvalue::Bucket>, keys: Vec<String>,
-    ) -> anyhow::Result<()> {
+        &mut self, bucket: Resource<bindings::Bucket>, keys: Vec<String>,
+    ) -> wasmtime::Result<Result<(), store::Error>> {
         tracing::debug!("BatchView::delete_many {keys:?}");
 
-        let jetstream = self.jetstream(&bucket)?;
+        let store = self.store(&bucket)?;
         for key in keys {
-            jetstream.delete(key).await?;
+            store.delete(key).await?;
         }
 
-        Ok(())
+        Ok(Ok(()))
     }
 }
 
-// Bucket holds a reference to the the NATS bucket. It is used to implement the
-// [`wasi_keyvalue::RuntimeBucket`] trait used by the messaging State.
-struct Bucket(jetstream::kv::Store);
+// Create a new Bucket for the specified NATS server.
+async fn new_bucket(
+    jetstream: &jetstream::Context, identifier: String,
+) -> anyhow::Result<bindings::Bucket> {
+    let inner = jetstream
+        .create_key_value(jetstream::kv::Config {
+            bucket: identifier.clone(),
+            history: 10,
+            ..Default::default()
+        })
+        .await?;
 
-impl Bucket {
-    // Create a new Bucket for the specified NATS server.
-    async fn new(jetstream: &jetstream::Context, identifier: String) -> anyhow::Result<Self> {
-        let inner = jetstream
-            .create_key_value(jetstream::kv::Config {
-                bucket: identifier.clone(),
-                history: 10,
-                ..Default::default()
-            })
-            .await?;
-
-        Ok(Self(inner))
-    }
-}
-
-// Implement the [`wasi_keyvalue::RuntimeBucket`] trait. The implementation
-// allows the wasi-keyvalue host to interact with the JetStream KV store.
-#[async_trait::async_trait]
-impl RuntimeBucket for Bucket {
-    fn as_any(&self) -> &dyn std::any::Any {
-        self
-    }
+    Ok(inner)
 }
