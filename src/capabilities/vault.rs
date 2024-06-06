3ecbbdf259173cfafa8c24f1e2a511ab6ddf0310
andrewweston <andrew.weston@propellerhead.co.nz>
2024-06-06 13:34:08 +1200

    add public keys

--- src/capabilities/vault.rs
+++ src/capabilities/vault.rs
@@ -34,11 +34,6 @@ mod bindings {
 
 pub type Error = anyhow::Error;
 
-// FIXME: replace hard-coded signer with key vault-based signing
-const ISSUER_DID: &str ="did:ion:EiDyOQbbZAa3aiRzeCkV7LOx3SERjjH93EXoIM3UoN4oWg:eyJkZWx0YSI6eyJwYXRjaGVzIjpbeyJhY3Rpb24iOiJyZXBsYWNlIiwiZG9jdW1lbnQiOnsicHVibGljS2V5cyI6W3siaWQiOiJwdWJsaWNLZXlNb2RlbDFJZCIsInB1YmxpY0tleUp3ayI6eyJjcnYiOiJzZWNwMjU2azEiLCJrdHkiOiJFQyIsIngiOiJ0WFNLQl9ydWJYUzdzQ2pYcXVwVkpFelRjVzNNc2ptRXZxMVlwWG45NlpnIiwieSI6ImRPaWNYcWJqRnhvR0otSzAtR0oxa0hZSnFpY19EX09NdVV3a1E3T2w2bmsifSwicHVycG9zZXMiOlsiYXV0aGVudGljYXRpb24iLCJrZXlBZ3JlZW1lbnQiXSwidHlwZSI6IkVjZHNhU2VjcDI1NmsxVmVyaWZpY2F0aW9uS2V5MjAxOSJ9XSwic2VydmljZXMiOlt7ImlkIjoic2VydmljZTFJZCIsInNlcnZpY2VFbmRwb2ludCI6Imh0dHA6Ly93d3cuc2VydmljZTEuY29tIiwidHlwZSI6InNlcnZpY2UxVHlwZSJ9XX19XSwidXBkYXRlQ29tbWl0bWVudCI6IkVpREtJa3dxTzY5SVBHM3BPbEhrZGI4Nm5ZdDBhTnhTSFp1MnItYmhFem5qZEEifSwic3VmZml4RGF0YSI6eyJkZWx0YUhhc2giOiJFaUNmRFdSbllsY0Q5RUdBM2RfNVoxQUh1LWlZcU1iSjluZmlxZHo1UzhWRGJnIiwicmVjb3ZlcnlDb21taXRtZW50IjoiRWlCZk9aZE10VTZPQnc4UGs4NzlRdFotMkotOUZiYmpTWnlvYUFfYnFENHpoQSJ9fQ";
-const VERIFY_KEY_ID: &str = "publicKeyModel1Id";
-const JWK_D: &str = "0Md3MhPaKEpnKAyKE498EdDFerD5NLeKJ5Rb-vC16Gs";
-
 pub struct Capability {}
 
 pub const fn new() -> Capability {
@@ -65,19 +60,33 @@ impl runtime::Capability for Capability {
 impl enclave::Host for State {
     // Sign the provided message using the signing key.
     async fn sign(&mut self, data: Vec<u8>) -> wasmtime::Result<Result<Vec<u8>, Resource<Error>>> {
-        // FIXME: replace hard-coded signer with key vault-based signing
+        // FIXME: replace hard-coded signer with key enclave-based signing
+        const JWK_D: &str = "0Md3MhPaKEpnKAyKE498EdDFerD5NLeKJ5Rb-vC16Gs";
+
         let decoded = Base64UrlUnpadded::decode_vec(JWK_D)?;
         let signing_key: ecdsa::SigningKey<Secp256k1> = ecdsa::SigningKey::from_slice(&decoded)?;
         let sig: ecdsa::Signature<Secp256k1> = signing_key.sign(&data);
         Ok(Ok(sig.to_vec()))
     }
 
-    async fn active_key(&mut self, op: KeyOp) -> wasmtime::Result<Result<Jwk, Resource<Error>>> {
-        todo!("return active key for KeyOp {op:?}")
+    async fn active_key(&mut self, _op: KeyOp) -> wasmtime::Result<Result<Jwk, Resource<Error>>> {
+        // FIXME: replace hard-coded public key with enclave-based key
+        Ok(Ok(Jwk {
+            crv: "secp256k1".into(),
+            kty: "EC".into(),
+            x: "tXSKB_rubXS7sCjXqupVJEzTcW3MsjmEvq1YpXn96Zg".into(),
+            y: Some("dOicXqbjFxoGJ-K0-GJ1kHYJqic_D_OMuUwkQ7Ol6nk".into()),
+        }))
     }
 
-    async fn next_key(&mut self, op: KeyOp) -> wasmtime::Result<Result<Jwk, Resource<Error>>> {
-        todo!("return next key for KeyOp {op:?}")
+    async fn next_key(&mut self, _op: KeyOp) -> wasmtime::Result<Result<Jwk, Resource<Error>>> {
+        // FIXME: replace hard-coded public key with enclave-based key
+        Ok(Ok(Jwk {
+            crv: "secp256k1".into(),
+            kty: "EC".into(),
+            x: "tXSKB_rubXS7sCjXqupVJEzTcW3MsjmEvq1YpXn96Zg".into(),
+            y: Some("dOicXqbjFxoGJ-K0-GJ1kHYJqic_D_OMuUwkQ7Ol6nk".into()),
+        }))
     }
 }
 
@@ -90,6 +99,10 @@ impl signer::Host for State {
     }
 
     async fn verification(&mut self) -> wasmtime::Result<VerificationMethod> {
+        // FIXME: get Issuer DID and Key ID from ...?
+        const ISSUER_DID: &str ="did:ion:EiDyOQbbZAa3aiRzeCkV7LOx3SERjjH93EXoIM3UoN4oWg:eyJkZWx0YSI6eyJwYXRjaGVzIjpbeyJhY3Rpb24iOiJyZXBsYWNlIiwiZG9jdW1lbnQiOnsicHVibGljS2V5cyI6W3siaWQiOiJwdWJsaWNLZXlNb2RlbDFJZCIsInB1YmxpY0tleUp3ayI6eyJjcnYiOiJzZWNwMjU2azEiLCJrdHkiOiJFQyIsIngiOiJ0WFNLQl9ydWJYUzdzQ2pYcXVwVkpFelRjVzNNc2ptRXZxMVlwWG45NlpnIiwieSI6ImRPaWNYcWJqRnhvR0otSzAtR0oxa0hZSnFpY19EX09NdVV3a1E3T2w2bmsifSwicHVycG9zZXMiOlsiYXV0aGVudGljYXRpb24iLCJrZXlBZ3JlZW1lbnQiXSwidHlwZSI6IkVjZHNhU2VjcDI1NmsxVmVyaWZpY2F0aW9uS2V5MjAxOSJ9XSwic2VydmljZXMiOlt7ImlkIjoic2VydmljZTFJZCIsInNlcnZpY2VFbmRwb2ludCI6Imh0dHA6Ly93d3cuc2VydmljZTEuY29tIiwidHlwZSI6InNlcnZpY2UxVHlwZSJ9XX19XSwidXBkYXRlQ29tbWl0bWVudCI6IkVpREtJa3dxTzY5SVBHM3BPbEhrZGI4Nm5ZdDBhTnhTSFp1MnItYmhFem5qZEEifSwic3VmZml4RGF0YSI6eyJkZWx0YUhhc2giOiJFaUNmRFdSbllsY0Q5RUdBM2RfNVoxQUh1LWlZcU1iSjluZmlxZHo1UzhWRGJnIiwicmVjb3ZlcnlDb21taXRtZW50IjoiRWlCZk9aZE10VTZPQnc4UGs4NzlRdFotMkotOUZiYmpTWnlvYUFfYnFENHpoQSJ9fQ";
+        const VERIFY_KEY_ID: &str = "publicKeyModel1Id";
+
         Ok(VerificationMethod {
             algorithm: Algorithm::Es256k,
             key_id: format!("{ISSUER_DID}#{VERIFY_KEY_ID}"),
