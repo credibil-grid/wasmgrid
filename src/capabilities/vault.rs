d3211e4df125b29fb7327941239e36d9aba4b2a8
andrewweston <andrew.weston@propellerhead.co.nz>
2024-07-27 16:40:24 +1200

    [skip] refactor vault (#5)

--- src/capabilities/vault.rs
+++ src/capabilities/vault.rs
@@ -5,9 +5,7 @@
 
 use anyhow::anyhow;
 use base64ct::{Base64UrlUnpadded, Encoding};
-use bindings::wasi::vault::enclave::{self, KeyOp};
-use bindings::wasi::vault::types::{self, Algorithm, Jwk, VerificationMethod};
-use bindings::wasi::vault::{signer, verifier};
+use bindings::wasi::vault::keystore::{self, Algorithm, Jwk, KeyType};
 use bindings::Vault;
 use ecdsa::signature::Signer as _;
 use k256::Secp256k1;
@@ -21,7 +19,7 @@ use crate::runtime::{self, Runtime, State};
 mod bindings {
     #![allow(clippy::future_not_send)]
 
-    pub use super::Error;
+    pub use super::{Error, KeySet};
 
     wasmtime::component::bindgen!({
         world: "vault",
@@ -30,12 +28,16 @@ mod bindings {
         async: true,
         trappable_imports: true,
         with: {
-            "wasi:vault/types/error": Error,
+            "wasi:vault/keystore/error": Error,
+            "wasi:vault/keystore/key-set": KeySet,
         },
     });
 }
 
 pub type Error = anyhow::Error;
+// pub type KeySet = async_nats::jetstream::kv::Store;
+
+pub struct KeySet {}
 
 pub struct Capability {}
 
@@ -60,9 +62,29 @@ impl runtime::Capability for Capability {
 }
 
 #[async_trait::async_trait]
-impl enclave::Host for State {
-    // Sign the provided message using the signing key.
-    async fn sign(&mut self, data: Vec<u8>) -> wasmtime::Result<Result<Vec<u8>, Resource<Error>>> {
+impl keystore::Host for State {
+    async fn open(
+        &mut self, _identifier: String,
+    ) -> wasmtime::Result<Result<Resource<KeySet>, Resource<Error>>> {
+        todo!()
+    }
+
+    async fn supported_algorithms(&mut self) -> wasmtime::Result<Vec<Algorithm>> {
+        todo!()
+    }
+}
+
+#[async_trait::async_trait]
+impl keystore::HostKeySet for State {
+    async fn generate(
+        &mut self, _rep: Resource<KeySet>, _: KeyType, _: Algorithm,
+    ) -> wasmtime::Result<Result<Jwk, Resource<Error>>> {
+        todo!("generate new key for KeyType")
+    }
+
+    async fn sign(
+        &mut self, _rep: Resource<KeySet>, _: KeyType, data: Vec<u8>,
+    ) -> wasmtime::Result<Result<Vec<u8>, Resource<Error>>> {
         // FIXME: replace hard-coded signer with key enclave-based signing
         const JWK_D: &str = "0Md3MhPaKEpnKAyKE498EdDFerD5NLeKJ5Rb-vC16Gs";
 
@@ -88,7 +110,9 @@ impl enclave::Host for State {
         Ok(Ok(sig.to_vec()))
     }
 
-    async fn active_key(&mut self, _op: KeyOp) -> wasmtime::Result<Result<Jwk, Resource<Error>>> {
+    async fn verifying_key(
+        &mut self, _rep: Resource<KeySet>, _: KeyType,
+    ) -> wasmtime::Result<Result<Jwk, Resource<Error>>> {
         // FIXME: replace hard-coded public key with enclave-based key
         Ok(Ok(Jwk {
             kid: None,
@@ -99,52 +123,32 @@ impl enclave::Host for State {
         }))
     }
 
-    async fn next_key(&mut self, _op: KeyOp) -> wasmtime::Result<Result<Jwk, Resource<Error>>> {
-        // FIXME: replace hard-coded public key with enclave-based key
-        Ok(Ok(Jwk {
-            kid: None,
-            kty: "EC".into(),
-            crv: "secp256k1".into(),
-            x: "tXSKB_rubXS7sCjXqupVJEzTcW3MsjmEvq1YpXn96Zg".into(),
-            y: Some("dOicXqbjFxoGJ-K0-GJ1kHYJqic_D_OMuUwkQ7Ol6nk".into()),
-        }))
-    }
-}
-
-#[async_trait::async_trait]
-impl signer::Host for State {
-    // Sign the provided message using the vault suite referenced by the
-    // verification-method.
-    async fn sign(&mut self, msg: Vec<u8>) -> wasmtime::Result<Result<Vec<u8>, Resource<Error>>> {
-        enclave::Host::sign(self, msg).await
-    }
-
-    async fn verification(&mut self) -> wasmtime::Result<VerificationMethod> {
-        // FIXME: get Issuer DID and Key ID from ...?
-        const ISSUER_DID: &str ="did:ion:EiDyOQbbZAa3aiRzeCkV7LOx3SERjjH93EXoIM3UoN4oWg:eyJkZWx0YSI6eyJwYXRjaGVzIjpbeyJhY3Rpb24iOiJyZXBsYWNlIiwiZG9jdW1lbnQiOnsicHVibGljS2V5cyI6W3siaWQiOiJwdWJsaWNLZXlNb2RlbDFJZCIsInB1YmxpY0tleUp3ayI6eyJjcnYiOiJzZWNwMjU2azEiLCJrdHkiOiJFQyIsIngiOiJ0WFNLQl9ydWJYUzdzQ2pYcXVwVkpFelRjVzNNc2ptRXZxMVlwWG45NlpnIiwieSI6ImRPaWNYcWJqRnhvR0otSzAtR0oxa0hZSnFpY19EX09NdVV3a1E3T2w2bmsifSwicHVycG9zZXMiOlsiYXV0aGVudGljYXRpb24iLCJrZXlBZ3JlZW1lbnQiXSwidHlwZSI6IkVjZHNhU2VjcDI1NmsxVmVyaWZpY2F0aW9uS2V5MjAxOSJ9XSwic2VydmljZXMiOlt7ImlkIjoic2VydmljZTFJZCIsInNlcnZpY2VFbmRwb2ludCI6Imh0dHA6Ly93d3cuc2VydmljZTEuY29tIiwidHlwZSI6InNlcnZpY2UxVHlwZSJ9XX19XSwidXBkYXRlQ29tbWl0bWVudCI6IkVpREtJa3dxTzY5SVBHM3BPbEhrZGI4Nm5ZdDBhTnhTSFp1MnItYmhFem5qZEEifSwic3VmZml4RGF0YSI6eyJkZWx0YUhhc2giOiJFaUNmRFdSbllsY0Q5RUdBM2RfNVoxQUh1LWlZcU1iSjluZmlxZHo1UzhWRGJnIiwicmVjb3ZlcnlDb21taXRtZW50IjoiRWlCZk9aZE10VTZPQnc4UGs4NzlRdFotMkotOUZiYmpTWnlvYUFfYnFENHpoQSJ9fQ";
-        const VERIFY_KEY_ID: &str = "publicKeyModel1Id";
-
-        Ok(VerificationMethod {
-            algorithm: Algorithm::Es256k,
-            key_id: format!("{ISSUER_DID}#{VERIFY_KEY_ID}"),
-            jwk: None,
-        })
-    }
-}
-
-#[async_trait::async_trait]
-impl verifier::Host for State {
-    async fn verify(
-        &mut self, _msg: Vec<u8>, _signature: Vec<u8>,
+    async fn delete(
+        &mut self, _rep: Resource<KeySet>, _: KeyType,
     ) -> wasmtime::Result<Result<(), Resource<Error>>> {
         todo!()
     }
+
+    async fn list_versions(
+        &mut self, _rep: Resource<KeySet>, _: KeyType,
+    ) -> wasmtime::Result<Result<Vec<Jwk>, Resource<Error>>> {
+        Ok(Ok(vec![Jwk {
+            kid: None,
+            kty: "EC".into(),
+            crv: "secp256k1".into(),
+            x: "tXSKB_rubXS7sCjXqupVJEzTcW3MsjmEvq1YpXn96Zg".into(),
+            y: Some("dOicXqbjFxoGJ-K0-GJ1kHYJqic_D_OMuUwkQ7Ol6nk".into()),
+        }]))
+    }
+
+    fn drop(&mut self, rep: Resource<KeySet>) -> Result<(), wasmtime::Error> {
+        tracing::debug!("keystore::HostKeySet::drop");
+        self.table().delete(rep).map_or_else(|e| Err(anyhow!(e)), |_| Ok(()))
+    }
 }
 
-impl types::Host for State {}
-
 #[async_trait::async_trait]
-impl types::HostError for State {
+impl keystore::HostError for State {
     async fn trace(&mut self, rep: Resource<Error>) -> wasmtime::Result<String> {
         tracing::warn!("FIXME: trace HostError");
         let error = self.table().get(&rep)?;
