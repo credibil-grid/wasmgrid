4bbde20594013bac321642aefe1e7dae86ffc85d
andrew_weston <andrew.weston@propellerhead.co.nz>
2025-08-20 13:15:18 +1200

    instrument attribute macro

--- examples/otel/src/lib.rs
+++ examples/otel/src/lib.rs
@@ -12,38 +12,38 @@ use wasi::http::types::{IncomingRequest, ResponseOutparam};
 struct HttpGuest;
 
 impl Guest for HttpGuest {
+    #[sdk_otel_attr::instrument]
     fn handle(request: IncomingRequest, response: ResponseOutparam) {
         // inject remote (host) context
         let now = SystemTime::now();
-        sdk_otel::instrument(|| {
-            tracing::debug!("telemetry initialized {:?}", now.elapsed().unwrap());
 
-            let meter = global::meter("my_meter");
-            let counter = meter.u64_counter("my_counter").build();
-            counter.add(1, &[KeyValue::new("key1", "value 1")]);
-            counter.add(1, &[KeyValue::new("key2", "value 2")]);
+        tracing::debug!("telemetry initialized {:?}", now.elapsed().unwrap());
 
-            // basic span
-            let tracer = global::tracer("basic");
-            tracer.in_span("main-operation", |cx| {
-                let span = cx.span();
-                span.set_attribute(KeyValue::new("my-attribute", "my-value"));
-                span.add_event("main span event", vec![KeyValue::new("foo", "1")]);
-                tracer.in_span("child-operation", |cx| {
-                    cx.span().add_event("sub span event", vec![KeyValue::new("bar", "1")]);
-                });
+        let meter = global::meter("my_meter");
+        let counter = meter.u64_counter("my_counter").build();
+        counter.add(1, &[KeyValue::new("key1", "value 1")]);
+        counter.add(1, &[KeyValue::new("key2", "value 2")]);
+
+        // basic span
+        let tracer = global::tracer("basic");
+        tracer.in_span("main-operation", |cx| {
+            let span = cx.span();
+            span.set_attribute(KeyValue::new("my-attribute", "my-value"));
+            span.add_event("main span event", vec![KeyValue::new("foo", "1")]);
+            tracer.in_span("child-operation", |cx| {
+                cx.span().add_event("sub span event", vec![KeyValue::new("bar", "1")]);
             });
-
-            let out = tracing::info_span!("handle request").in_scope(|| {
-                tracing::info!("received request");
-                let router = Router::new().route("/", post(handle));
-                sdk_http::serve(router, request)
-            });
-
-            tracing::info!("request processed {:?}", now.elapsed().unwrap());
-
-            ResponseOutparam::set(response, out);
         });
+
+        let out = tracing::info_span!("handle request").in_scope(|| {
+            tracing::info!("received request");
+            let router = Router::new().route("/", post(handle));
+            sdk_http::serve(router, request)
+        });
+
+        tracing::info!("request processed {:?}", now.elapsed().unwrap());
+
+        ResponseOutparam::set(response, out);
     }
 }
 
